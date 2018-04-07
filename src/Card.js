8f7b7dcc7c0038d7572613f6c9543520ac235e07
loneklingon <theanthonyking@outlook.com>
2018-04-06 20:06:11 -0400

    fixed screen size

--- src/Card.js
+++ src/Card.js
@@ -20,7 +20,7 @@ class ACard extends Component
     
         <div> 
         
-            <Card className="grow ma2 shadow-1 modifyH cc pa2">
+            <Card className="grow shadow-1 modifyH cc pa2 h-100 mb3" width ="10">
                 <CardImg top width = "25%" src={this.props.robot.id} alt="Robo Pic" />
         
                 <CardTitle className= "tc ma2"> Name: {this.props.robot.name}</CardTitle>
